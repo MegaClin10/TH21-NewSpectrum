8b5499b58c97a3e140719ac5f5577f428756d676
Ryan Hall <ryan.hall.tx@gmail.com>
2021-01-30 17:12:59 -0600

    Updated both scripts
    Added both everything and headlines as triggerable scripts that use a string query

--- scripts/get_news_everything.py
+++ scripts/get_news_everything.py
@@ -3,13 +3,22 @@ from newsapi import NewsApiClient
 import json
 
 def get_news_everything(query):
-    in_database = check_database_existing(query)
-    print(in_database)
-    #if in_database:
+    newsapi = NewsApiClient(api_key=grab_credentials('news-api-key'))
+
+    data = check_database_existing(query)
+    in_database = data["in_database"]
+    db = data["database"]
+
+    if in_database:
         # return database
-    #else:
+        return list(data["documents"])
+    else:
         # perform search
+        all_articles = newsapi.get_everything(q=query, sort_by="relevancy", language='en')
+
         # add to database
+        all_articles["query"] = query.lower()
+        db['cached-news'].insert_one(all_articles)
 
 def check_database_existing(query):
     # check the database
@@ -18,15 +27,19 @@ def check_database_existing(query):
     collection=db['cached-news']
     cursor = collection.find({'query': query})
 
-    return cursor.count() > 0
+    data = {
+        "in_database": cursor.count() > 0,
+        "database" : db,
+        "documents" : cursor
+    }
+
+    return data
 
 def connect_to_mongo_db():
-    client = MongoClient("mongodb+srv://worker:" + grab_credentials() + "@development.zukcn.mongodb.net/TAMUhack?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://worker:" + grab_credentials("mongo-service-key") + "@development.zukcn.mongodb.net/TAMUhack?retryWrites=true&w=majority")
     db = client.TAMUhack
     return db
 
-def grab_credentials():
+def grab_credentials(key):
     secrets = json.load(open("../secrets.json"))
-    return secrets["mongo-service-key"]
-
-get_news_everything("BITCOIN".lower())
+    return secrets[key]
